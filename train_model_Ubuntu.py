6dcd970a4b0a237e818181aac1a4c4708414a107
Weitheng Haw <wei.thenghaw@disneystreaming.com>
2024-12-19 22:30:58 +0000

    undo temp change and remove transformer

--- train_model_Ubuntu.py
+++ train_model_Ubuntu.py
@@ -37,11 +37,6 @@ def main():
             'labels_dir': 'labels/train',
             'classes': dataset_config['names'],
             'input_dim': (640, 640),
-            'transforms': [
-                {'RandomHorizontalFlip': {'p': 0.5}},
-                {'RandomScale': {'scale_range': (0.8, 1.2)}},
-                {'RandomCrop': {'scale_range': (0.8, 1.0)}}
-            ]
         },
         dataloader_params={
             'batch_size': hw_params['batch_size'],
